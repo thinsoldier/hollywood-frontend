ef38c3005b391d8136155622e7e69cb7d72f03df
thinsoldier <thinsoldier@thinsoldier.com>
2016-11-05 00:13:59 -0600

    More progress on parsing the contents of the data folder.

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?
+error_reporting( E_ALL );
+
 // Import useful utility functions
 include('php/functions/func.debug.php');
 
@@ -18,11 +20,65 @@ function _listDirectoryContents( $path )
 	return $found;
 }
 
-$data1 = _listDirectoryContents('./data/');
-showme($data1, 'root of data folder');
+function _parseDirectorData( $directorToken )
+{
+	$data = _listDirectoryContents('./data/'.$directorToken.'/');
+	
+	$name = str_replace('-',' ',$directorToken);
+	$name = ucwords($name);
+	
+	$photoKey = array_search('photo.jpg', $data['files']);
+	if( $photoKey === false )
+	{ $photo = 'no-photo.jpg'; }
+	else 
+	{ 
+		$photo = 'data/'.$directorToken.'/photo.jpg';
+		unset( $data['files'][$photoKey] );
+	}
+	
+	$structure = array('token'=>$directorToken,'name'=>$name, 'photo'=>$photo,'movies'=>array());
 
-$data2 = _listDirectoryContents('./data/wes-anderson/');
-showme($data2, 'wes anderson folder');
+	// set up initial array for each movie
+	foreach( $data['files'] as $filename )
+	{
+		$parts = explode('.',$filename);
+		$token = $parts[0];
+		$name = str_replace('-',' ',$token);
+		$name = ucwords($name);
+
+		$structure['movies'][ $token ] = array('token'=>$token, 'name'=>$name, 'description'=>null, 'poster'=>'no-poster.jpg');
+	}
+
+	// loop through again and fill in real values for description file and poster file
+	foreach( $data['files'] as $filename )
+	{	
+		$parts = explode('.',$filename);
+		$token = $parts[0];
+		
+		if( $parts[1] === 'md' )
+		{
+			$structure['movies'][ $token ]['description'] = 'data/'.$directorToken.'/'.$filename;
+		}
+		
+		if( $parts[1] === 'jpg'  )
+		{
+			$structure['movies'][ $token ]['poster'] = 'data/'.$directorToken.'/'.$filename;
+		}
+		
+	}
+	return $structure;
+}
+
+$dataRootRaw = _listDirectoryContents('./data/');
+showme($dataRootRaw, 'Contents in root of data folder:');
+
+$dataParsed = array();
+foreach( $dataRootRaw['folders'] as $directorToken )
+{
+	$dataParsed[ $directorToken ] = _parseDirectorData( $directorToken );
+}
+
+showme($dataParsed, 'Data from parsing all director folders within data folder:');
 
 include('template.html');
 ?>
